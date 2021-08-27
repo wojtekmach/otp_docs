af6538bf197083a106e9c130701fa9a63d38de5b
Wojtek Mach <wojtek@wojtekmach.pl>
2021-08-27 15:26:01 +0200

    up

--- docs.sh
+++ docs.sh
@@ -16,9 +16,12 @@ vsn=$(cat vsn.mk | grep VSN | head -1 | awk -F ' +' '{print $3}')
 pwd
 ex_doc $app $vsn preloaded/ebin -o $pwd/docs/$app --config $pwd/docs.config
 
+echo "<li><a href=\"https://wojtekmach.pl/otp_docs/erts\">erts</a></li>" > $pwd/docs/index.html
+
 for i in $root/**/; do
   cd $i
   app=$(basename $i)
   vsn=$(cat vsn.mk | grep VSN | head -1 | awk -F ' +' '{print $3}')
   ex_doc $app $vsn ebin/ -o $pwd/docs/$app --config $pwd/docs.config
+  echo "<li><a href=\"https://wojtekmach.pl/otp_docs/$app\">$app</a></li>" >> $pwd/docs/index.html
 done
